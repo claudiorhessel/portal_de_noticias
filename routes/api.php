1d4f1f174a4d987ce0f4e90f02f0f6b15e950a60
Claudio Hessel <claudiorhessel@gmail.com>
2023-04-03 13:11:09 -0300

    feat: create api routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthorController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,58 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'v1'], function() {
+    Route::group(['middleware' => ['auth:sanctum']], function() {
+        Route::group(['prefix' => 'user'], function() {
+            Route::get('/', function (Request $request) {
+                return $request->user();
+            });
+        });
+
+        Route::group(['prefix' => 'author'], function() {
+            Route::post('/', [FileApiController::class, 'store']);
+            Route::put('/{id}', [FileApiController::class, 'update']);
+            Route::delete('/{id}', [FileApiController::class, 'destroy']);
+        });
+
+        Route::group(['prefix' => 'news'], function() {
+            Route::post('/', [FileApiController::class, 'store']);
+            Route::put('/{id}', [FileApiController::class, 'update']);
+            Route::delete('/{id}', [FileApiController::class, 'destroy']);
+        });
+
+        Route::group(['prefix' => 'settings'], function() {
+            Route::post('/', [FileApiController::class, 'store']);
+            Route::put('/{id}', [FileApiController::class, 'update']);
+            Route::delete('/{id}', [FileApiController::class, 'destroy']);
+        });
+    });
+
+    Route::group(['prefix' => 'author'], function() {
+        Route::get('/', [FileApiController::class, 'index']);
+        Route::get('/{id}', [FileApiController::class, 'show']);
+    });
+
+    Route::group(['prefix' => 'news'], function() {
+        Route::get('/', [FileApiController::class, 'index']);
+        Route::get('/{id}', [FileApiController::class, 'show']);
+    });
+
+    Route::group(['prefix' => 'settings'], function() {
+        Route::get('/', [FileApiController::class, 'index']);
+        Route::get('/{id}', [FileApiController::class, 'show']);
+    });
+
+    Route::group(['prefix' => 'auth'], function() {
+        Route::post('register', [AuthController::class, 'createUser']);
+        Route::get('login', [AuthController::class, 'loginUser']);
+    });
 });
+
+Route::get('unauthorized', function () {
+    return response()->json(['error' => 'Unauthorized.'], 401);
+})->name('unauthorized');
+
+Route::any('{segment}', function () {
+    return response()->json(['error' => 'Bad request.'], 400);
+})->where('segment', '.*');
