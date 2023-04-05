6b99a62ee329feab6e7d96249d40ae3be381b91c
Claudio Hessel <claudiorhessel@gmail.com>
2023-04-04 21:02:37 -0300

    fix: path to portal.css file

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -8,7 +8,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" ></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="{!! url('/assets/tinymce/js/tinymce/tinymce.min.js') !!}"></script>
-    <link rel="stylesheet" href="/css/portal.css">
+    <link rel="stylesheet" href="{!! url('/css/portal.css') !!}">
     <title>@yield('title', 'Portal de Notícias')</title>
 </head>
 <body>
