84163bd2d666ea9a8c63269be53dfc3ab7dfb0b5
Claudio Hessel <claudiorhessel@gmail.com>
2023-04-05 09:37:57 -0300

    fix: index route redirect to portal route

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/portal');
 });
 
 Route::get('/portal', [PortalController::class, 'index'])->name('portal.index');
